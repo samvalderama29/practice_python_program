c35efafb8110ec0292a2a9d9a2b26289cd9ac468
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-01 10:49:23 +0800

    Added Prog07.py

---
+++ Prog07.py
@@ -0,0 +1,8 @@
+# Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
+
+num1 = int(input("First number: "))
+num2 = int(input("Second number: "))
+
+raisenum = num1**num2
+
+print(raisenum)
