27a2edf75e67ce8fd1a7feb6c52282eb3c285714
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-01 10:20:49 +0800

    Added Prog03.py

---
+++ Prog03.py
@@ -0,0 +1,10 @@
+# Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+
+num1 = int(input("First number: "))
+num2 = int(input("Second number: "))
+
+sumnum = num1 + num2
+
+print(sumnum)
+
+
